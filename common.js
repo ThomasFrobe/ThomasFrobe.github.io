886def32190a321cfb150a41c19991ddc2cf534b
ThomasFrobe <thomasfrobe@gmail.com>
2024-12-03 13:03:27 -0500

    Add files via upload

--- common.js
+++ common.js
@@ -24,3 +24,38 @@ function checkCardPosition() {
 
 // Run once on load
 checkCardPosition();
+
+// cartUtility.js
+document.addEventListener("DOMContentLoaded", function () {
+    updateCartCount();
+});
+
+function updateCartCount() {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    let totalQuantity = 0;
+
+    cartItems.forEach(item => {
+        totalQuantity += item.quantity;
+    });
+
+    // Get or create the red circle for the cart count
+    let cartIcon = document.querySelector(".shoplink");
+    if (!cartIcon) return;
+
+    let countCircle = document.querySelector(".cartCountCircle");
+    if (!countCircle) {
+        countCircle = document.createElement("div");
+        countCircle.classList.add("cartCountCircle");
+        cartIcon.appendChild(countCircle);
+    }
+
+    // Update the count or hide it if zero
+    if (totalQuantity > 0) {
+        countCircle.innerText = totalQuantity;
+        countCircle.style.display = "flex";
+        cartIcon.classList.add("stuff")
+    } else {
+        countCircle.style.display = "none";
+        cartIcon.classList.remove("stuff")
+    }
+}
